86952403ee790cb975792db8dd18e8a47a840bd5
GREPY <janocko@fathamir.sk>
2012-12-09 20:19:16 +0100

    Doplnené binary operators

--- src/expression/binop/BinaryOperator.java
+++ src/expression/binop/BinaryOperator.java
@@ -1,5 +1,5 @@
 package expression.binop;
 
 public enum BinaryOperator {
-	PLUS,MINUS,MULT,DIV//...
+	PLUS, MINUS, MULT, DIV, MOD, BSLEFT, BSRIGHT, BAND, BXOR, BOR, AND, OR, GT, LT, GET, LET, EQ, NEQ
 }
