3f32503afea70ead62bede650fa7e1e81d8acaa8
Martin Kolinek <martin.kolinek@gmail.com>
2012-12-10 16:51:48 +0100

    Upravene FunctionCallExpression, aby mohol mat viac parametrov

--- src/expression/FunctionCallExpression.java
+++ src/expression/FunctionCallExpression.java
@@ -1,9 +1,20 @@
 package expression;
 
+import java.util.ArrayList;
+
 public class FunctionCallExpression implements Expression {
 
+	public FunctionCallExpression(String name) {
+		args=new ArrayList<Expression>();
+		this.name=name;
+	}
+	
 	public String name;
-	public Expression args;
+	public ArrayList<Expression> args;
+	
+	public void addExp(Expression exp){
+		args.add(exp);
+	}
 	
 	@Override
 	public void accept(ExpressionVisitor v) {
