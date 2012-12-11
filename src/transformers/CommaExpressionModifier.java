f603e7a73210da2b57ad96caf6e73791340b9785
GREPY <janocko@fathamir.sk>
2012-12-11 14:39:19 +0100

    CommaExpressionModifier
    Pre vyraz s jednou ciarkou, vrat obycajny Expression

---
+++ src/transformers/CommaExpressionModifier.java
@@ -0,0 +1,15 @@
+package transformers;
+
+import expression.CommaExpression;
+
+public class CommaExpressionModifier extends EmptyExpressionModifier {
+	
+	public void visit(CommaExpression ce){
+		if (ce.expressions.size() == 1){
+			result = ce.expressions.get(0);
+		} else {
+			result = ce;
+		}
+	}
+
+}
