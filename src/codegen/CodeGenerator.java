1969ecd94217d61e51f79eac73251291833d4902
Jakub Kováč <jkfmfi@gmail.com>
2012-12-11 13:05:24 +0100

    generovanie kodu vetvenia
    potrebujem vediet ako sa bude celz kod generovat, co bude v ktorej
    premennej a tak

--- src/codegen/CodeGenerator.java
+++ src/codegen/CodeGenerator.java
@@ -11,4 +11,14 @@ public class CodeGenerator {
 	{
 		return localCounter++;
 	}
+	
+	//<TODO
+	public string vetvenie(Condition c,Block b1,Block b2)//neviem ako sa budu volat typy, toto je navrh struktury
+	{
+		generator.visit(c);//tu bude treba zapisat potrebny kod na vyhodnotenie podmienky a do c.premenna meno premennej s jej hodnotou  
+		generator.visit(b1);//do b1.label treba label statemenu b1 
+		generator.visit(b2);//podobne ako b2
+		return "br " || c.premenna || " " || b1.label || " " || b2.label; //syntax celkom neviem ale takto nejak
+	}
+	//TODO>
 }
