d0f9dd9e098cbb033f98ec94c79e21434ea5bbb8
GREPY <janocko@fathamir.sk>
2012-12-11 10:48:11 +0100

    FunctionArgsModifier, UnaryChargeModifier
    Rozbalenie argumentov funkcie, zjednodušenie výrazu so znamienkom

---
+++ src/transformers/UnaryChargeModifier.java
@@ -0,0 +1,20 @@
+package transformers;
+
+import expression.binop.BinaryExpression;
+import expression.binop.BinaryOperator;
+import expression.constant.IntConstantExpression;
+import expression.unop.UnaryExpression;
+import expression.unop.UnaryOperator;
+
+public class UnaryChargeModifier extends EmptyExpressionModifier {
+	
+	public void visit(UnaryExpression ue){
+		if (ue.op == UnaryOperator.PLUS){
+			result = ue.exp;
+		}
+		if (ue.op == UnaryOperator.MINUS){
+			result = new BinaryExpression(new IntConstantExpression(0), BinaryOperator.MINUS, ue.exp);
+		}
+	}
+
+}
