8cb49df248b40f307f0d86aa1d879359c6d4e3bc
GREPY <janocko@fathamir.sk>
2012-12-19 10:07:25 +0100

    InitializerModifier

--- src/modifiers/InitializerModifier.java
+++ src/modifiers/InitializerModifier.java
@@ -1,13 +1,55 @@
 package modifiers;
 
+import java.util.ArrayList;
+
 import declaration.ResolvedDeclaration;
+import declaration.initializer.CompoundInitializer;
+import declaration.initializer.DesignatedInitializer;
+import declaration.initializer.ExpressionInitializer;
+import declaration.initializer.Initializer;
+import expression.Expression;
+import expression.IDExpression;
+import expression.IndexingExpression;
+import expression.MemberAccessExpression;
+import expression.binop.BinaryExpression;
+import expression.binop.BinaryOperator;
+import statements.OneexpressionStatement;
+import statements.Statement;
 import transformers.EmptyBlockModifier;
 
 public class InitializerModifier extends EmptyBlockModifier {
 	@Override
 	public void visit(ResolvedDeclaration i) {
+		ArrayList<Statement> stats = getStatements(i.initializer, new IDExpression(i.identifier));
 		
 		i.initializer = null;
 		result.add(i);
+		
+		for (Statement statement : stats) {
+			result.add(statement);
+		}
+	}
+	
+	private ArrayList<Statement> getStatements(Initializer init, Expression context){
+		ArrayList<Statement> stats = new ArrayList<Statement>();
+		if (init instanceof ExpressionInitializer){
+			//expression
+			stats.add(new OneexpressionStatement(new BinaryExpression(context, BinaryOperator.ASSIG, ((ExpressionInitializer)init).exp)));
+		} else {
+			//compound
+			for (DesignatedInitializer di : ((CompoundInitializer)init).initializers){
+				if (di.designator.expr != null){
+					//indexing
+					stats.addAll(getStatements(di.initializer, new IndexingExpression(context, di.designator.expr)));
+				} else if (di.designator.id != null){
+					//member
+					stats.addAll(getStatements(di.initializer, new MemberAccessExpression(context, di.designator.id)));
+				} else {
+					assert false; //toto by nemalo nastat
+					//default indexing
+				}
+			}
+		}
+		return stats;
 	}
 }
