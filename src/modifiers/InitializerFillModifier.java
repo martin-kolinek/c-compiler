aee084c22d08a2ef9272a7298ca811d6e211a01e
GREPY <janocko@fathamir.sk>
2012-12-20 09:38:58 +0100

    InitializerFillModifier - fuj :P
    * Na vrchnej urovni inicializera zoradi indexy a vyriesi nullove
    designatory
    * Zatial nevie ist na nizsiu uroven
    * Treba otestovat, bol by to zazrak, keby to fungovalo

--- src/modifiers/InitializerFillModifier.java
+++ src/modifiers/InitializerFillModifier.java
@@ -9,16 +9,24 @@ import declaration.initializer.DesignatedInitializer;
 import declaration.initializer.Designator;
 import declaration.initializer.ExpressionInitializer;
 import declaration.initializer.Initializer;
+import expression.constant.IntConstantExpression;
 import transformers.EmptyBlockModifier;
 import types.ArrayType;
 import types.StructType;
 import types.Type;
 
+/**
+ * 
+ * Na vrchnej urovni inicializera zoradi indexy a vyriesi nullove designatory
+ * Zatial nevie ist na nizsiu uroven
+ * Treba otestovat, bol by to zazrak, keby to fungovalo
+ * 
+ * @author GREPY
+ *
+ */
 public class InitializerFillModifier extends EmptyBlockModifier {
 	@Override
 	public void visit(ResolvedDeclaration i) {
-		//ArrayList<Statement> stats = getStatements(i.initializer, new IDExpression(i.identifier));
-		
 		i.initializer = fillSortInitializer(i.initializer, i.type);
 		
 		result.add(i);
@@ -31,6 +39,10 @@ public class InitializerFillModifier extends EmptyBlockModifier {
 			//one expression - do nothing
 		} else {
 			//compound
+			//first, fill and sort all children initializers //NEVIEM
+			ArrayList<DesignatedInitializer> dis = new ArrayList<DesignatedInitializer>();
+			
+			//then fill and sort this initializer
 			if (typ instanceof StructType){
 				//expect structure
 				HashMap<String, Boolean> used_members = new HashMap<String, Boolean>(); 
@@ -45,7 +57,7 @@ public class InitializerFillModifier extends EmptyBlockModifier {
 					i++;
 				}
 				
-				ArrayList<DesignatedInitializer> dis = new ArrayList<DesignatedInitializer>();
+				
 				//create null initializers
 				for (int j=0; j<i; j++){
 					DesignatedInitializer di = new DesignatedInitializer();
@@ -75,10 +87,58 @@ public class InitializerFillModifier extends EmptyBlockModifier {
 						used_members.put(di.designator.id, true);
 					}
 				}
+				
 			} else if (typ instanceof ArrayType){
 				//expect array
-				//@TODO: Ako zoradit initializery, ak ich index je expression?
+				int count = 0;
+				Long highest = new Long(0);
+				Boolean all_ints = true;
+				for (DesignatedInitializer di : ((CompoundInitializer)init).initializers){
+					if (di.designator.expr instanceof IntConstantExpression){
+						if ((((IntConstantExpression)di.designator.expr).value) > highest)
+							highest = ((IntConstantExpression)di.designator.expr).value;
+					} else {
+						all_ints = false;
+					}
+					count++;
+				}
+				
+				if (all_ints){ //if all indexes are integers (or nulls), sort and fill
+					
+					Long max_index = Math.max(highest+1, count);
+					
+					//create null initializers
+					for (int j=0; j<max_index; j++){
+						DesignatedInitializer di = new DesignatedInitializer();
+						di.designator = new Designator(new IntConstantExpression(j));
+						di.initializer = null;
+						dis.add(di);
+					}
+					
+					//fill initializers
+					for (DesignatedInitializer di : ((CompoundInitializer)init).initializers){
+						if (di.designator.expr != null){
+							dis.set((int) ((IntConstantExpression)di.designator.expr).value, di);
+						}
+					}
+					
+					//not specified keys
+					for (DesignatedInitializer di : ((CompoundInitializer)init).initializers){
+						if (di.designator.expr == null){
+							//find first next null initializer
+							int ind = 0;
+							while (dis.get(ind).initializer != null){ 
+								ind++;
+							}
+							di.designator.expr = new IntConstantExpression(ind); //set apropriate designator id
+							dis.set(ind, di); //replace
+						}
+					}
+					
+				}
+				
 			}
+			((CompoundInitializer)init).initializers = dis;
 		}
 		return init;
 	}
